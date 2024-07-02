7f4f6cdc0e658acabe2e4fb00b266d647bd31cd4
Miguel Edward Rosal <miguelrosal12@gmail.com>
2024-07-02 11:24:47 +0800

    Added a second screen

---
+++ lib/secondscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MySecondScreen extends StatelessWidget {
+  const MySecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
